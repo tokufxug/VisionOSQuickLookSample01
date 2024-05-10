e70a3b66e88e564d8222dd4a4803d43f7c6b77f1
tokufxug <tokufxug@gmail.com>
2024-05-10 11:30:00 +0900

    Initial Commit

---
+++ VisionOSQuickLookSample01/VisionOSQuickLookSample01App.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSQuickLookSample01App.swift
+//  VisionOSQuickLookSample01
+//
+//  Created by Sadao Tokuyama on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSQuickLookSample01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
